f1e85ea67c7c1288301d93e170077675bad0187f
ready4exam <ready4urexam@gmail.com>
2025-12-17 10:36:15 +0530

    chore: update table_id for New Empires and Kingdoms → social_new_kingdoms_5_quiz

--- js/curriculum.js
+++ js/curriculum.js
@@ -211,7 +211,7 @@ export const curriculum = {
       },
       {
         "chapter_title": "New Empires and Kingdoms",
-        "table_id": "",
+        "table_id": "social_new_kingdoms_5_quiz",
         "section": "Social Science"
       },
       {
