fa448f483d9a479984d4b94c2c20c526a0f1d1c4
ready4exam <ready4urexam@gmail.com>
2025-12-17 10:32:57 +0530

    chore: update table_id for Major Landforms of the Earth → social_major_earth_5_quiz

--- js/curriculum.js
+++ js/curriculum.js
@@ -248,7 +248,7 @@ export const curriculum = {
       },
       {
         "chapter_title": "Major Landforms of the Earth",
-        "table_id": "",
+        "table_id": "social_major_earth_5_quiz",
         "section": "Social Science"
       },
       {
