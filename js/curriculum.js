2606df23977f2a797037745c32039e59ae87bbe8
ready4exam <ready4urexam@gmail.com>
2025-12-17 10:27:40 +0530

    chore: update table_id for On The Trail of the Earliest People → social_trail_people_5_quiz

--- js/curriculum.js
+++ js/curriculum.js
@@ -166,7 +166,7 @@ export const curriculum = {
       },
       {
         "chapter_title": "On The Trail of the Earliest People",
-        "table_id": "",
+        "table_id": "social_trail_people_5_quiz",
         "section": "Social Science"
       },
       {
